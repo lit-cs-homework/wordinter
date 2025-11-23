8086eaafa5e631fce8d50f4f87c91df705ba447f
litlighilit <litlighilit@foxmail.com>
2025-11-23 09:47:54 +0800

    bump-version: 1.1.6 (fixup CD typo)

--- src/main.h
+++ src/main.h
@@ -11,7 +11,7 @@
 
 #define Author "litlighilit(lcm)"
 #define License "GPLv3"
-#define Version "1.1.5"
+#define Version "1.1.6"
 
 #define Proj "WordInter"
 #define Desc "a file-based search engine"
